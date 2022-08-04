24587c088dfa918ab5146c31e31276a42650a83d
Irina <balabanova.iu@gmail.com>
2022-08-04 15:00:41 +0300

    add history calculator

--- calculator/main.js
+++ calculator/main.js
@@ -1,7 +1,9 @@
 window.onload = function () {
   const items = document.querySelectorAll(".item"),
-    input = document.querySelector(".input");
+    input = document.querySelector(".input"),
+    result = document.querySelector(".result");
   let command = "";
+  let results = [];
 
   items.forEach((item) => {
     item.addEventListener("click", () => {
@@ -9,11 +11,15 @@ window.onload = function () {
       if (value == "C") {
         command = "";
       } else if (value == "=") {
+        let tmp = command;
         command = eval(command);
+        results.push(`${tmp} = ${command}`);
       } else {
         command += value;
       }
       setInputValue(command);
+      const tmp = makeList(results);
+      displayList(tmp);
     });
   });
 
@@ -24,10 +30,43 @@ window.onload = function () {
   input.addEventListener("input", (event) => {
     command = input.value;
   });
+
   document.addEventListener("keypress", (event) => {
     if (event.code == "Enter") {
+      let tmp = command;
       command = eval(command);
+      results.push(`${tmp} = ${command}`);
       setInputValue(command);
+      makeListResuls(results);
     }
   });
+
+  function makeListResuls(str) {
+    const template = `<li>${str}</li>`;
+    return template;
+  }
+
+  // function makeList() {
+  //   // let arr = [];
+  //   // for (let i = 0; i < results.length; i++) {
+  //   //   let template = makeListResuls(results[i]);
+  //   //   arr.push(template);
+  //   // }
+  //   // const str = arr.join("");
+  //   let template = `<ul>${str}</ul>`;
+  //   return template;
+  // }
+
+  function makeList() {
+    let template = `<ul>${results
+      .map((item) => {
+        return `<li>${item}</li>`;
+      })
+      .join("")}</ul>`;
+    return template;
+  }
+
+  function displayList(template) {
+    result.innerHTML = template;
+  }
 };
